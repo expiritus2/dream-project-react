5f866cd46af43c36d6ca5facf141ec34b08ad22a
Michail <michaildrazdovich@gmail.com>
2018-12-25 00:12:38 +0300

    add useTranslate hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,2 @@
 export { default as useRedux } from "./use-redux";
+export { default as useTranslate } from "./use-translate";
---
+++ src/hooks/use-translate/index.js
@@ -0,0 +1,37 @@
+import { useCallback, useContext } from "react";
+import TranslationContext from "context/translation";
+import translations from "../../en";
+
+export default () => {
+  const locale = useContext(TranslationContext);
+
+  const translate = useCallback(
+    (key, interpolation) => {
+      if (typeof key !== "string") {
+        throw new Error("A key with type string must be specified");
+      }
+      const keys = key.split(".");
+      let index = 0;
+      let currentKey = translations;
+      while (keys.length !== index) {
+        currentKey = currentKey[keys[index]] ? currentKey[keys[index]] : key;
+        index += 1;
+      }
+      if (typeof interpolation !== "undefined") {
+        if (typeof interpolation !== "object") {
+          throw new Error("Interpolation must be an object");
+        }
+        Object.keys(interpolation).forEach(interKey => {
+          currentKey = currentKey.replace(
+            new RegExp(`%{${interKey}}`),
+            interpolation[interKey],
+          );
+        });
+      }
+      return currentKey || key;
+    },
+    [locale],
+  );
+
+  return [translate, locale];
+};
