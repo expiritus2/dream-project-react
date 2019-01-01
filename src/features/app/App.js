1f1c25022d5c5d000576e822ae04f89f62bcea3c
Michail <michaildrazdovich@gmail.com>
2019-01-01 14:45:10 +0300

    add some prop types

--- src/features/app/App.js
+++ src/features/app/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { bool } from "prop-types";
 import User from "features/user";
 import Anonymous from "features/anonymous";
 
 const App = ({ isLoggedIn }) => {
   return isLoggedIn ? <User /> : <Anonymous />;
+};
+
+App.propTypes = {
+  isLoggedIn: bool.isRequired,
 };
 
 export default App;
